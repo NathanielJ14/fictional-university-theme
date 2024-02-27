db82a248ad0bd3e08434fec9ae8ac4bff5bb8198
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-02-27 16:57:41 -0500

    Set up styles and dynamic info

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
         while(have_posts()) {
             the_post(); ?>
             <div class="post_item">
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h2 class="headline headline--medium headline--post-title" ><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <div class="metabox" >
-                    <p>Posted By on in</p>
+                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n-j-y'); ?> in <?php echo get_the_category_list(', '); ?></p>
                 </div>
                 <div class="generic-content">
                     <?php the_excerpt(); ?>
