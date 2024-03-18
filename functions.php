9dc9591cde9a98ada8df1dc364260fc42aff5e96
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-03-18 15:41:52 -0400

    Add more perameters

--- functions.php
+++ functions.php
@@ -25,7 +25,11 @@
     //     register_post_type('event', array(
     //         'public' => true,
     //         'labels' => array(
-    //             'name' => 'Events'
+    //             'name' => 'Events',
+    //             'add_new_item' => 'Add New Event',
+    //             'edit_item' => 'Edit Event',
+    //             'all_items' => 'All Events',
+    //             'singular_name' => 'Event'
     //         ),
     //         'menu_icon' => 'dashicons-calendar'
     //     ));
